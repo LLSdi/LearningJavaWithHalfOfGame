de4df1234485109056401d858ddd3a3ea374f82f
LLSdi <43058629+LLSdi@users.noreply.github.com>
2020-03-17 12:47:53 +0800

    Test github

---
+++ ForTest/src/main/java/ATest.java
@@ -0,0 +1,9 @@
+/**
+ * @author halfOfGame
+ * @create 2020-03-17,12:47
+ */
+public class ATest {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+    }
+}
