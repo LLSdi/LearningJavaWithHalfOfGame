e7431c2672dcc8dd45debfd10cb699d1006b8940
LLSdi <43058629+LLSdi@users.noreply.github.com>
2020-03-18 11:18:18 +0800

    Add some class

--- ForTest/src/main/java/ATest.java
+++ ForTest/src/main/java/ATest.java
@@ -4,6 +4,7 @@
  */
 public class ATest {
     public static void main(String[] args) {
-        System.out.println("Hello World!!");
+        char[] chs = new char[20];
+        System.out.println(chs.length);
     }
 }
