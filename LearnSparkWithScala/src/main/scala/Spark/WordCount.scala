f22757354d10249604f93cc5866416268305b88b
halfofgame <halfofgame@foxmail.com>
2020-06-21 10:05:17 +0800

    add some classes.

--- LearnSparkWithScala/src/main/scala/Spark/WordCount.scala
+++ LearnSparkWithScala/src/main/scala/Spark/WordCount.scala
@@ -4,12 +4,12 @@ import org.apache.spark.{SparkConf, SparkContext}
 
 object WordCount {
   def main(args: Array[String]): Unit = {
-    val conf = new SparkConf().setAppName("CombineAndRemoveRepetition").setMaster("local")
-    val sc = new SparkContext(conf)
-
-    val inputFile = ""
-    val textFile = sc.textFile(inputFile)
-    val wordCount = textFile.flatMap(line => line.split(" ")).map(word => (word, 1)).reduceByKey((a, b) => a + b)
-    wordCount.foreach(println)
+//    val conf = new SparkConf().setAppName("CombineAndRemoveRepetition").setMaster("local")
+//    val sc = new SparkContext(conf)
+//
+//    val inputFile = ""
+//    val textFile = sc.textFile(inputFile)
+//    val wordCount = textFile.flatMap(line => line.split(" ")).map(word => (word, 1)).reduceByKey((a, b) => a + b)
+//    wordCount.foreach(println)
   }
 }
